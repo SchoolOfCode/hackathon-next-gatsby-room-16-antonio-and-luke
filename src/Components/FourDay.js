ca40276619d6723f11d2775a4d4e7d4f833ae833
lukechaplin <lukechaplin@gmail.com>
2022-01-28 17:07:49 +0000

    fixeds four day error

--- src/Components/FourDay.js
+++ src/Components/FourDay.js
@@ -8,7 +8,7 @@ const FourDay = () => {
     "Friday",
     "Saturday",
   ];
-  var tomorrow = new Date();
+  var today = new Date();
   tomorrow.setDate(today.getDate() + 3);
   let day = weekday[today.getDay()];
   return day;
